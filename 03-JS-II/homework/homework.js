3f4cda17eda96d072760c61c7707cd2083ed7b65
Joaquingro <joaquinsgro@gmail.com>
2022-09-07 16:53:16 -0500

    Verifying code

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -128,7 +128,7 @@ function fizzBuzz(numero) {
     return "fizz";
   } if (numero % 5 === 0){
     return "buzz"
-  } if (numero % 3 === 0 && numero % 5 === 0){
+  } if (numero % 15 === 0){
     return "fizzbuzz";
   }
   return numero;
@@ -143,7 +143,7 @@ function operadoresLogicos(num1, num2, num3) {
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
   if(num1 > num2 && num1 > num3 && num1 > 0){
     return "Número 1 es mayor y positivo";
-  } if (num1 < 0 && num2 < 0 && num3 < 0){
+  } if (num1 < 0 || num2 < 0 || num3 < 0){
     return "Hay negativos";
   } if(num3 > num1 && num3 > num2){
     num3++;
