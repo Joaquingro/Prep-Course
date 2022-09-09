491ac090fbdde1d249b561bf572fd0bc3513e55d
Joaquingro <joaquinsgro@gmail.com>
2022-09-08 20:46:35 -0500

    Finish homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -138,15 +138,13 @@ if(arguments.length < 1) return 0;
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-  let contador = 0;
-  for(let i = 0; i<arreglo.lenght; i++){
+   let contador = 0;
+  for (let i = 0; i < arreglo.length ; i++) {
     if(arreglo[i] > 19){
-      contador++;
+      contador++
+    }
   }
-  
-  }
-return contador; 
-
+  return contador
 }
 
 
