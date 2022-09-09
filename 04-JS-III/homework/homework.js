79ee108a9b4507d302d53c541ebf2c28dd135348
Joaquingro <joaquinsgro@gmail.com>
2022-09-08 20:39:26 -0500

    Finish exercises bucles

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -143,9 +143,9 @@ function cuentoElementos(arreglo){
     if(arreglo[i] > 18){
       contador++;
   }
-  return contador; 
+  
   }
-
+return contador; 
 
 }
 
@@ -157,12 +157,11 @@ function diaDeLaSemana(numeroDeDia) {
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
   
-    if( numeroDeDia === 1 || numeroDeDia === 7 ){
-      return "Es fin de semana";
-    }
-    return "Es día laboral";
+   if(numeroDeDia === 1 || numeroDeDia === 7 ){
+    return "Es fin de semana"
   }
-
+  return "Es dia Laboral"
+} 
 
 
 
@@ -182,14 +181,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  for( var i=0; i<arreglo.lenght-1; i++){
+ for (var i =0 ; i < arreglo.length - 1; i++) {
     if(arreglo[i] !== arreglo[i+1]){
-        return false;
+      return false
+    }
   }
-
-  }
-  return true;
-
+  return true
 } 
 
 
