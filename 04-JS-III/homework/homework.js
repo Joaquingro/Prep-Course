bdb46c34db269d281c819e648fcda9bcf354a0d8
Joaquingro <joaquinsgro@gmail.com>
2022-09-08 19:26:04 -0500

    Finish exercises bucles

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -245,9 +245,9 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
-  breakStatement(numero){
+  breakStatement(numero)
     if(i === 5) continue;
-  }
+  
   }
 
 
