33259d99c29d0d332110b743bffb9cf912c8a841
Joaquingro <joaquinsgro@gmail.com>
2022-09-13 20:45:42 -0500

    VERIFYING

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -10,13 +10,14 @@ function crearUsuario() {
   // Tu código:
   function Usuario(opciones){
    
-      this.usuario =  opciones.usuario,
-      this.nombre = opciones.nombre,
-      this.email = opciones.email,
-      this.password = opciones.password,
+      this.usuario =  opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password;
       
     
   }
+
   Usuario.prototype.saludar =  function(){
         return 'Hola, mi nombre es ' + this.nombre;
       }
