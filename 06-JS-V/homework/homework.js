9eddd6055b1652ca4f259e360e0c08b762795222
Joaquingro <joaquinsgro@gmail.com>
2022-09-13 20:29:56 -0500

    VERIFYING

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -77,7 +77,10 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Devolver la nueva persona creada
   class NuevaPersona extends Persona {
       contructor (nombre, apellido, edad, dir){
-        super(nombre, apellido, edad, dir);
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.dir = dir;
 
       }
   }
