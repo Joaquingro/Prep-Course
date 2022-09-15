9fadb2ccee71e6ef0fb3c0981ef4e591b8c4322b
Joaquingro <joaquinsgro@gmail.com>
2022-09-14 20:48:11 -0500

    VERIFYING

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,7 +4,7 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  return nombre[0].toUpperCase() + nombre.slice(0); 
+  return nombre[0].toUpperCase() + nombre.slice(1); 
  }
 
 function invocarCallback(cb) {
@@ -26,18 +26,18 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
-  var suma = numeros.reduce(function(acc,numeros){
-    cb = acc + numeros;
-  });
+  var suma = numeros.reduce(function(acc,numeros2){
+    cb = acc + numeros2;
+  }, 0);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  array.forEach(function(cb){
-    array = cb; 
-  });
+  for(var i= 0; i< array.lenght; i++){
+      cb(array[i]); 
+  }
 }
 
 function map(array, cb) {
@@ -45,17 +45,23 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  var nuevoArray = array.map(function(array, cb){
-      array = cb; 
-      nuevoArray = cb;
+  var nuevoArray = array.map(function(dc){
+      return cb(dc);
   });
-
+  return nuevoArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var nuevoArray = [];
+  for( var i=0; i<array.lenght; i++ ){
+    if(array[i][0] === "a"){
+      nuevoArray.push(array[i]) 
+    }
+  }
+  return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
